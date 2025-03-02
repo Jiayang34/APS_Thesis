27a27abb97cde9e69ca3717c8f028e7e82a33821
Jiayang34 <zhangjy0304@gmail.com>
2025-03-02 05:32:53 +0100

    CIFAR10-H function and APS result

--- src/__init__.py
+++ src/__init__.py
@@ -4,10 +4,16 @@ from .saps import *
 from .raps_hyp_opt import *
 from .saps_hyp_opt import *
 from .temperature_scaling import *
+from .aps_cifar10h import *
 
 __all__ = [lambda_optimization_raps, lambda_optimization_saps, k_reg_optimization,  # HypOpt.py
            ModelWithTemperature,  # Temperature Scaling
            split_data_set, aps_scores, aps_classification, eval_aps,  # aps
            raps_scores, raps_classification,  # raps
-           saps_scores, saps_classification   # saps
+           saps_scores, saps_classification,   # saps
+           # CIFAR10-H
+           split_data_set_cifar10h, aps_classification_cifar10h, raps_classification_cifar10h,
+           saps_classification_cifar10h,
+           aps_scores_cifar10h, raps_scores_cifar10h, saps_scores_cifar10h,
+           eval_aps_cifar10h
            ]
